1963eb111c20b4eaad562b726ef2596b76f37ff6
Riley Scheurman <riley.scheurman@gmail.com>
2017-11-23 11:58:49 -0700

    Implemented the initial nava bar along with updated layout to accomidate

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+  
   resources :repair_requests
 
   root to: 'pages#home'
