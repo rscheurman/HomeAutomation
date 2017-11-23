94317a489b64f0b2896d770635f5414cf2590b7f
Riley Scheurman <riley.scheurman@gmail.com>
2017-11-22 23:20:00 -0700

    Implemented the pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+
   resources :repair_requests
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
 end
